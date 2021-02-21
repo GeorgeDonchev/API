db676b9b41f45af0f012287e432ce26ed4b8d794
George Donchev <70439491+GeorgeDonchev@users.noreply.github.com>
2021-02-21 09:32:35 +0200

    Added API demos

---
+++ tv_credits.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = 'eb3a7d37cbc42bb0f1598c41b1384cc7'
+
+"""https://api.themoviedb.org/3/movie/550?api_key=eb3a7d37cbc42bb0f1598c41b1384cc7"""
+
+person_id = 5
+base_url = "https://api.themoviedb.org/3"
+end_path = f'/person/{person_id}/tv_credits'
+end_point = f'{base_url}{end_path}?api_key={api_key}'
+
+r = requests.get(end_point)
+data = r.json()
+# print(data.keys())
+# print(data['id'])
+cast_data = data['cast']
+for cast in cast_data:
+    print(cast.keys())
+    print()
